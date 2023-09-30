478f5ac66c5b17c2199aed8477324582a8753701
DaffaAdityaDev <daffaaditya990@gmail.com>
2023-09-30 13:59:01 +0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CS
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
-import { AudioPlayerProvider } from './context/AudioPlayerProvider'
+import AudioPlayerProvider from './context/AudioPlayerProvider'
 export default function App({ Component, pageProps }: AppProps) {
   return  (
     <AudioPlayerProvider>
